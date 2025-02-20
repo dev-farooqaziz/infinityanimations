b0aadfbcc68aa7b548e148ad8c409e48700c138a
dev-farooqaziz <dev.farooqaziz@gmail.com>
2025-02-21 04:14:36 +0500

    changes

--- src/app/game-lp/components/Contact/Contact.jsx
+++ src/app/game-lp/components/Contact/Contact.jsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from 'react'
 import Teliphone from "media/game-lp/number.png"
 import Email from "media/game-lp/email.png"
 import Location from "media/game-lp/location.png"
+import Payment from "media/healthcare/payment.png"
 import BtnBg2 from "media/game-lp/formbtn.png"
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaVimeo, FaYoutube } from 'react-icons/fa'
 
@@ -189,52 +190,33 @@ const Contact = () => {
                     <div className="grid grid-cols-12 items-center gap-x-6 5xl:gap-x-0 gap-y-8">
                         <div className="col-span-12 lg:col-span-7">
                             <div className="txt">
-                                <h2 className='text-[30px] sm:text-[40px] lg:text-[50px] 5xl:text-[60px] leading-[50px] sm:leading-[50px] lg:leading-[60px] 5xl:leading-[90px] font-semibold text-white poppins mb-3'>Let’s Create Your Next
+                                <h2 className='text-[30px] sm:text-[40px] lg:text-[50px] leading-[50px] sm:leading-[50px] lg:leading-[60px] font-semibold text-white poppins mb-3'>Let’s Create Your Next
                                     Hit Game Together</h2>
                                 <p className='text-[13px] sm:text-[15px] xl:text-[18px] leading-[25px] sm:leading-[30px] xl:leading-[33px] text-white poppins mb-7 lg:mb-10 lg:w-11/12 xl:pr-5'>Our mobile games deliver immersive experiences with intuitive controls and engaging gameplay. Designed for all devices, they offer seamless performance and stunning visuals.</p>
                                 <div className="details">
-                                    <div className="flex flex-wrap items-center justify-between pr-14 5xl:w-[75%]">
-                                        <Link href="tel:;" className="flex items-center justify-center gap-4  mb-4 lg:mb-8">
-                                            <Image src={Teliphone} alt='Number' />
-                                            <span className='text-[14px] xl:text-[17px] leading-[24px] xl:leading-[27px] font-normal poppins'>012-345-6789</span>
-                                        </Link>
-                                        <Link href="mailto:info@xyz.com" className="flex items-center justify-center gap-4  mb-4 lg:mb-8">
-                                            <Image src={Email} alt='Email' />
-                                            <span className='text-[14px] xl:text-[17px] leading-[24px] xl:leading-[27px] font-normal poppins'>info@xyz.com</span>
-                                        </Link>
-                                    </div>
-                                    <Link href="mailto:info@xyz.com" className="flex items-center gap-4 mb-4 lg:mb-7">
+                                    <Link href="javascript:;" className="flex items-center gap-4 mb-4 lg:mb-7">
                                         <Image src={Location} alt='Location' />
                                         <span className='text-[14px] xl:text-[17px] leading-[24px] xl:leading-[27px] font-normal poppins'>Office abc street, London</span>
                                     </Link>
-                                    <Link href="mailto:info@xyz.com" className="flex items-center gap-4 mb-4 lg:mb-7">
+                                    <Link href="javascript:;" className="flex items-center gap-4 mb-4 lg:mb-7">
                                         <Image src={Location} alt='Location' />
                                         <span className='text-[14px] xl:text-[17px] leading-[24px] xl:leading-[27px] font-normal poppins'>Office abc street, Canada</span>
                                     </Link>
-                                    <Link href="mailto:info@xyz.com" className="flex items-center gap-4 mb-4 lg:mb-7">
+                                    <Link href="javascript:;" className="flex items-center gap-4 mb-4 lg:mb-7">
                                         <Image src={Location} alt='Location' />
                                         <span className='text-[14px] xl:text-[17px] leading-[24px] xl:leading-[27px] font-normal poppins'>Office abc street, New York</span>
                                     </Link>
+                                    <Link href="tel:;" className="flex items-center gap-4  mb-4 lg:mb-8">
+                                        <Image src={Teliphone} alt='Number' />
+                                        <span className='text-[14px] xl:text-[17px] leading-[24px] xl:leading-[27px] font-normal poppins'>012-345-6789</span>
+                                    </Link>
+                                    <Link href="mailto:info@xyz.com" className="flex items-center gap-4  mb-4 lg:mb-8">
+                                        <Image src={Email} alt='Email' />
+                                        <span className='text-[14px] xl:text-[17px] leading-[24px] xl:leading-[27px] font-normal poppins'>info@xyz.com</span>
+                                    </Link>
                                 </div>
-                                <div className="socials_icons flex items-center gap-4 lg:gap-6 pt-5">
-                                    <Link href="#href" className='group rounded-[5px] bg-[#2f2e3c] w-[50px] h-[50px] flex items-center justify-center'>
-                                        <FaFacebook className='text-[25px] group-hover:scale-[1.1] group-hover:duration-1000 ease-in-out duration-1000' />
-                                    </Link>
-                                    <Link href="#href" className='group rounded-[5px] bg-[#2f2e3c] w-[50px] h-[50px] flex items-center justify-center'>
-                                        <FaTwitter className='text-[25px] group-hover:scale-[1.1] group-hover:duration-1000 ease-in-out duration-1000' />
-                                    </Link>
-                                    <Link href="#href" className='group rounded-[5px] bg-[#2f2e3c] w-[50px] h-[50px] flex items-center justify-center'>
-                                        <FaInstagram className='text-[25px] group-hover:scale-[1.1] group-hover:duration-1000 ease-in-out duration-1000' />
-                                    </Link>
-                                    <Link href="#href" className='group rounded-[5px] bg-[#2f2e3c] w-[50px] h-[50px] flex items-center justify-center'>
-                                        <FaLinkedin className='text-[25px] group-hover:scale-[1.1] group-hover:duration-1000 ease-in-out duration-1000' />
-                                    </Link>
-                                    <Link href="#href" className='group rounded-[5px] bg-[#2f2e3c] w-[50px] h-[50px] flex items-center justify-center'>
-                                        <FaYoutube className='text-[25px] group-hover:scale-[1.1] group-hover:duration-1000 ease-in-out duration-1000' />
-                                    </Link>
-                                    {/* <Link href="#href" className='group rounded-[5px] bg-[#2f2e3c] w-[50px] h-[50px] flex items-center justify-center'>
-                                        <FaVimeo className='text-[25px] group-hover:scale-[1.1] group-hover:duration-1000 ease-in-out duration-1000' />
-                                    </Link> */}
+                                <div>
+                                    <Image src={Payment} alt='Payment-Methods' className='w-8/12 sm:w-5/12 lg:w-6/12 mt-5 lg:mt-8 object-contain' />
                                 </div>
                             </div>
                         </div>
